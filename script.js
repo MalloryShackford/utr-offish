9c6932f71f00ec2b3eb25b519f02ffaf6804648c
MShackford <93612680+MalloryShackford@users.noreply.github.com>
2023-06-22 12:20:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ checkbox.addEventListener('change', () => {
     document.body.classList.toggle('dark')
 
 });
+
+// Tidio Chatbot
+
+document.tidioIdentify = {
+  distinct_id: "unique_id", // Unique visitor ID in your system
+  email: "contact@mail", // visitor email
+  name: "John Doe", // Visitor name
+  phone: "+44 2032897807" //Visitor phone
+};
